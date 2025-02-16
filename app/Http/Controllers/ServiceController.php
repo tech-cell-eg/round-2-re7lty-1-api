e0a62c2ad584e70f9a9421f2e95c26e65778b300
sara mohamed <sara666.s47@gamil.com>
2025-02-16 21:35:59 +0200

    list all services

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
+use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+
+    use ApiResponseTrait;
+    public function index()
+    {
+        try {
+            $services = Service::all();
+
+            return $this->successResponse($services, "Services retrieved successfully!");
+        } catch (\Exception $e) {
+            return $this->errorResponse("Failed to retrieve services", 500);
+        }
+    }
 }
