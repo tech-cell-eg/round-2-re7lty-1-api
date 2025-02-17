dde5ee193d15f70bc7dde230e812ba5056b66d20
sara mohamed <sara666.s47@gamil.com>
2025-02-17 18:52:45 +0200

    edtied contact us controller

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactUsRequest;
 use App\Mail\ContactUsMail;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
@@ -11,18 +12,12 @@ class ContactUsController extends Controller
 {
     use ApiResponseTrait;
 
-    public function send(Request $request)
+    public function store(ContactUsRequest $request)
     {
-        $request->validate([
-            'name'    => 'required|string|max:255',
-            'email'   => 'required|email',
-            'message' => 'required|string|min:10',
-        ]);
-
         try {
             $data = $request->only(['name', 'email', 'message']);
 
-            Mail::to('sara666.s47@gmail.com')->send(new ContactUsMail($data));
+            Mail::to(env('CONTACT_US_EMAIL'))->send(new ContactUsMail($data));
 
             return $this->successResponse(null, "Your message has been sent successfully!");
         } catch (\Exception $e) {
